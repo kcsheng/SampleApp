060d4cf0d09b918d78e9c819515ca57569ada550
kcsheng <kacysheng@mail.com>
2020-09-30 14:21:34 +1000

    Set up title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Returns the full tile of the page
+  def title(page_title = "")
+    base_title = "KC Sheng's Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+  
 end
