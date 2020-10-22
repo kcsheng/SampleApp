d5a9b9cc565a83225822d93bc0f7847ab11be62a
kcsheng <kacysheng@mail.com>
2020-10-22 16:42:35 +1100

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -18,4 +20,29 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  # Returns a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user by assigning a remember digest into user's database
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Check whether user's remember token matches digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    # Same as BCrypt::Password.new(remember_digest) == remember_token
+  end
+
+  # Forget a user by setting the remember digest to nil
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  
+  
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,4 +17,20 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  # Log in as a particular user
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
 end
+
+class ActionDispatch::IntegrationTest # ActiveSupport::TestCase is only inherited by models. We need to write a helper that can be inherited by this class for integration test, so we can use it in users login test, which is inside integration test.
+
+  # Log in as a particular user
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email, 
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
+end
+
